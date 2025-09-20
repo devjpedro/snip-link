ad146fdf4f5e6798b23d4b8d791cab025aef4cfb
buckz <joaopedross.ti9@gmail.com>
2025-09-20 16:18:28 -0300

    chore: added tsconfig package

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,7 +2,7 @@ import { Elysia } from "elysia";
 
 const PORT = 3333;
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(PORT);
+const app = new Elysia().get("/", () => "hi").listen(PORT);
 
 // biome-ignore lint/suspicious/noConsole: <Necessary for logging>
 console.log(
