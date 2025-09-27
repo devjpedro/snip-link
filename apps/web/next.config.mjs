4e9043f485c7cd7f3565dcae7dc79612a7143bca
buckz <joaopedross.ti9@gmail.com>
2025-09-27 14:30:25 -0300

    fix: testing fix in vercel deploy

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@snip-link/ui"],
+  experimental: {
+    serverMinification: false,
+  },
 };
 
 export default nextConfig;
