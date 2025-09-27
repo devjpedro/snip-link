ebefa06c3597df661ca95b61b12f89f27a9166d5
buckz <joaopedross.ti9@gmail.com>
2025-09-27 14:43:59 -0300

    fix: loading envs in build command

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@snip-link/ui"],
-  experimental: {
-    serverMinification: false,
-  },
 };
 
 export default nextConfig;
