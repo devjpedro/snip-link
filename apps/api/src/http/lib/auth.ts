30c8a9dc4e82bffcc61c03e164eb43f9a90cc921
buckz <joaopedross.ti9@gmail.com>
2025-10-07 20:07:47 -0300

    chore: added a pre commit and fix env in build

--- apps/api/src/http/lib/auth.ts
+++ apps/api/src/http/lib/auth.ts
@@ -19,7 +19,7 @@ export const auth = betterAuth({
     database: {
       generateId: false,
     },
-    useSecureCookies: env.NODE_ENV === "production",
+    useSecureCookies: process.env.NODE_ENV === "production",
     crossSubDomainCookies: {
       enabled: true,
     },
