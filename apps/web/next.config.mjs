1c59764a9906539f8eacedcf616267c038aa2062
buckz <joaopedross.ti9@gmail.com>
2025-10-30 12:25:14 -0300

    fix: error dependencies

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@snip-link/ui"],
+  // serverExternalPackages: ["bcrypt", "pg", "pg-pool"],
+  // experimental: {
+  //   serverComponentsExternalPackages: ["bcrypt", "pg", "pg-pool"],
+  // },
 };
 
 export default nextConfig;
